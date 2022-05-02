35a79adb4ed0e917ae7ca004cbc7057cff468dc6
IGuijarroJ <iguijarro3@gmail.com>
2022-05-02 21:20:04 +0200

    "Creando componentes iniciales"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HomeComponent } from './home/home/home.component';
+import { ShellComponent } from './core/shell/shell.component';
+import { HeaderComponent } from './core/shell/header/header.component';
+import { MainComponent } from './core/shell/main/main.component';
+import { FooterComponent } from './core/shell/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent,
+    ShellComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BarraNavegacionComponent } from './shell/header/barra-navegacion/barra-navegacion.component';
+
+
+
+@NgModule({
+  declarations: [
+    BarraNavegacionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
